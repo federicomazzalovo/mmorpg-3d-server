3b1cb0fcef32875abce8575642a11a07abdf12b8
Andrea Crispo <andrea.crispo.cr@gmail.com>
2020-12-07 17:02:07 +0100

    Add files via upload

---
+++ src/main/java/my/plaground/Game.java
@@ -0,0 +1,13 @@
+package my.plaground;
+
+/**
+ * Hello world!
+ *
+ */
+public class Game
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
