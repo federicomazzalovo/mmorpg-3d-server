1d49594d3059f0ec5cc6bfb5ea17e0f36eb3d4e4
Andrea <andrea.crispo.cr@gmail.com>
2021-01-03 23:48:02 +0100

    Added position updated api

--- src/main/java/my/plaground/Controller/PositionController.java
+++ src/main/java/my/plaground/Controller/PositionController.java
@@ -2,18 +2,41 @@ package my.plaground.Controller;
 
 
 import lombok.extern.slf4j.Slf4j;
+import my.plaground.Character;
 import my.plaground.Position;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import static my.plaground.SimpleRpgKataApplication.getCharacterList;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/position")
 public class PositionController {
 
-    @GetMapping("/{playerId}")
-    public Position getPlayerPosition(String playerId) {
-        return new Position(0,0);
+    @GetMapping("/{characterId}")
+    public ResponseEntity<Position> getCharacterPosition(@PathVariable Integer characterId) throws Exception {
+        Character character = getCharacterList().stream()
+                .filter(c -> c.getId() == characterId)
+                .findFirst()
+                .orElseThrow(() -> new Exception("Employee not found for this id :: " + characterId));
+
+        return ResponseEntity.ok(character.getPosition());
+
+    }
+
+
+    @PutMapping("/{characterId}")
+    public ResponseEntity<Position> updateCharacterPosition(@PathVariable Integer characterId, @RequestBody Position newPosition) throws Exception {
+        Character character = getCharacterList().stream()
+                .filter(c -> c.getId() == characterId)
+                .findFirst()
+                .orElseThrow(() -> new Exception("Employee not found for this id :: " + characterId));
+
+        character.setPosition(newPosition);
+        return ResponseEntity.ok(newPosition);
+
     }
 }
--- src/test/java/my/plaground/Controller/PositionControllerTest.java
+++ src/test/java/my/plaground/Controller/PositionControllerTest.java
@@ -2,6 +2,7 @@ package my.plaground.Controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import my.plaground.Character;
+import my.plaground.Position;
 import my.plaground.SimpleRpgKataApplication;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -26,7 +28,7 @@ class PositionControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Disabled
+
     @Test public void
     ensure_that_position_is_correct_for_specific_char() {
         try {
@@ -36,7 +38,23 @@ class PositionControllerTest {
             String jsonResult = result.getResponse().getContentAsString();
 
             assertEquals(objectMapper.writeValueAsString(characterToFind.getPosition()), jsonResult);
-            assertEquals(HttpStatus.OK, result.getResponse().getStatus());
+            assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Disabled
+    @Test public void
+    ensure_that_position_is_correctly_updated() {
+        try {
+            Character characterToFind = SimpleRpgKataApplication.getCharacterList().stream().filter(c -> c.getId() == 1).findFirst().orElse(null);
+            Position newPosition = Position.at(characterToFind.getPosition().getX()+2, characterToFind.getPosition().getY()+ 3);
+            MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.put("/api/position/1", newPosition)).andReturn();
+            String jsonResult = result.getResponse().getContentAsString();
+
+            assertEquals(objectMapper.writeValueAsString(newPosition), jsonResult);
+            assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
         } catch (Exception e) {
             e.printStackTrace();
         }
