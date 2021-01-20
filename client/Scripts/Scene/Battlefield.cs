f2d5fe7ed6d804de4e3c785201224df1ec25d935
Federico Mazzalovo <mazzalovo.federico@gmail.com>
2021-01-20 21:57:42 +0100

    Added logic to get characters from server and render them

--- client/Scripts/Scene/Battlefield.cs
+++ client/Scripts/Scene/Battlefield.cs
@@ -1,21 +1,49 @@
 using Godot;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+public class Character
+{
+	public long InitHp { get; set; }
+	public long Hp { get; set; }
+	public long Id { get; set; }
+	public long Level { get; set; }
+	public long Resistance { get; set; }
+	public CharacterPosition Position { get; set; }
+	public bool Alive { get; set; }
+	public bool Dead { get; set; }
+}
+
+public class CharacterPosition
+{
+	public long X { get; set; }
+	public long Y { get; set; }
+}
 
 public class Battlefield : Node2D
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
-
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
-	}
+		using (HttpClient client = new HttpClient())
+		{
+			HttpResponseMessage response = client.GetAsync("http://localhost:8080/api/character/all").Result;
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+			string result = response.Content.ReadAsStringAsync().Result;
+
+			List<Character> characters = JsonConvert.DeserializeObject<List<Character>>(result);
+
+			Sprite sprite = this.GetNode("Sprite") as Sprite;
+
+			foreach (Character character in characters)
+			{
+				Sprite characterSprite = sprite.Duplicate() as Sprite;
+				characterSprite.Position = new Vector2(character.Position.X, character.Position.Y);
+				characterSprite.Show();
+
+				this.AddChild(characterSprite);
+			}
+		}
+	}
 }
