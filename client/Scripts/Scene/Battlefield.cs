a0befb2a8cc6340f1b3a03f4494d4d8a020c7c57
Andrea <andrea.crispo.cr@gmail.com>
2021-01-20 23:25:43 +0100

    Added characterNode and logic to move player

--- client/Scripts/Scene/Battlefield.cs
+++ client/Scripts/Scene/Battlefield.cs
@@ -7,72 +7,73 @@ using System.Net.Http;
 
 public class Character
 {
-    public long InitHp { get; set; }
-    public long Hp { get; set; }
-    public long Id { get; set; }
-    public long Level { get; set; }
-    public long Resistance { get; set; }
-    public CharacterPosition Position { get; set; }
-    public bool Alive { get; set; }
-    public bool Dead { get; set; }
+	public long InitHp { get; set; }
+	public long Hp { get; set; }
+	public long Id { get; set; }
+	public long Level { get; set; }
+	public long Resistance { get; set; }
+	public CharacterPosition Position { get; set; }
+	public bool Alive { get; set; }
+	public bool Dead { get; set; }
+
+	public bool IsPlayer { get; set; }
 }
 
 public class CharacterPosition
 {
-    public long X { get; set; }
-    public long Y { get; set; }
+	public long X { get; set; }
+	public long Y { get; set; }
 }
 
 public class Battlefield : Node2D
 {
-    private Sprite spriteToClone;
-    private IEnumerable<Character> characters;
-    private Character myCharacter;
+	private KinematicBody2D playerNode;
+	private KinematicBody2D genericNodeToClone;
+	private IEnumerable<Character> characters;
+	private Character player;
 
-    public override void _Ready()
-    {
-        this.spriteToClone = this.GetNode("Sprite") as Sprite;
+	public override void _Ready()
+	{
+		this.genericNodeToClone = this.GetNode("CharacterNode") as KinematicBody2D;
+		this.playerNode = this.GetNode("PlayerNode") as KinematicBody2D;
 
-        this.characters = this.RetrieveCharacters();
-        this.LoadCharacters();
-        this.AddCharactersSprites();
-    }
+		this.characters = this.RetrieveCharacters();
+		this.LoadCharacters();
+		this.AddCharactersSprites();
+	}
 
-    public override void _Input(InputEvent @event)
-    {
-        base._Input(@event);
-    }
 
-    private void LoadCharacters()
-    {
-        this.myCharacter = this.characters.FirstOrDefault();
-        this.characters = this.characters.Where(character => character.Id != this.myCharacter.Id);        
-    }
+	private void LoadCharacters()
+	{
+		this.player = this.characters.SingleOrDefault(character => character.IsPlayer);
+		this.characters = this.characters.Where(character => !character.IsPlayer);        
+	}
 
-    private void AddCharactersSprites()
-    {
-        foreach (Character character in this.characters)
-            this.AddCharacterSprite(character);
-    }
+	private void AddCharactersSprites()
+	{
+		this.playerNode.Position = new Vector2(this.player.Position.X, this.player.Position.Y);
+		foreach (Character character in this.characters)
+			this.AddCharacterSprite(character);
+	}
 
-    private void AddCharacterSprite(Character character)
-    {
-        Sprite characterSprite = this.spriteToClone.Duplicate() as Sprite;
-        characterSprite.Position = new Vector2(character.Position.X, character.Position.Y);
-        characterSprite.Show();
+	private void AddCharacterSprite(Character character)
+	{
+		KinematicBody2D characterSprite = this.genericNodeToClone.Duplicate() as KinematicBody2D;
+		characterSprite.Position = new Vector2(character.Position.X, character.Position.Y);
+		characterSprite.Show();
 
-        this.AddChild(characterSprite);
-    }
+		this.AddChild(characterSprite);
+	}
 
-    private List<Character> RetrieveCharacters()
-    {
-        using (HttpClient client = new HttpClient())
-        {
-            HttpResponseMessage response = client.GetAsync("http://localhost:8080/api/character/all").Result;
+	private List<Character> RetrieveCharacters()
+	{
+		using (HttpClient client = new HttpClient())
+		{
+			HttpResponseMessage response = client.GetAsync("http://localhost:8080/api/character/all").Result;
 
-            string json = response.Content.ReadAsStringAsync().Result;
+			string json = response.Content.ReadAsStringAsync().Result;
 
-            return JsonConvert.DeserializeObject<List<Character>>(json);
-        }
-    }
+			return JsonConvert.DeserializeObject<List<Character>>(json);
+		}
+	}
 }
