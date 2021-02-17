fb0cb7f93b4ce29f2c46224e132d84801eabff60
Andrea <andrea.crispo.cr@gmail.com>
2021-02-18 00:27:53 +0100

    Added missing file

---
+++ client/Network/WebSocketParams.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplerpgkataclient.Network
+{
+    public class WebSocketParams
+    {
+        public long characterId { get; set; }
+        public float positionX { get; set; } 
+        public float positionY { get; set; }
+    }
+}
