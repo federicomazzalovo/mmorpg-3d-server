8d47937c9dd1c7628d2edb442c11c7c0e4b0718c
Andrea <andrea.crispo.cr@gmail.com>
2021-02-24 22:40:25 +0100

    Added login scene

---
+++ client/Scripts/Scene/Session.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplerpgkataclient.Scripts.Scene
+{
+    public static class Session
+    {
+        public static string Username { get; set; }
+    }
+}
