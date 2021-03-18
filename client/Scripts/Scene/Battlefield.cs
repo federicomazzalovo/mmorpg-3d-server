dd871c1c12c2e2dc983b75dda1e0829e151c2ffe
Andrea <andrea.crispo.cr@gmail.com>
2021-03-19 00:11:46 +0100

    Review logic for connected users

--- client/Scripts/Scene/Battlefield.cs
+++ client/Scripts/Scene/Battlefield.cs
@@ -11,206 +11,208 @@ using System.Text;
 
 public enum CharacterClass
 {
-    Unknown = 0,
-    Paladin = 1,
-    Wizard = 2,
-    Rogue = 3
+	Unknown = 0,
+	Paladin = 1,
+	Wizard = 2,
+	Rogue = 3
 }
 
 
 public class Character
 {
-    public double InitHp { get; set; }
-    public double Hp { get; set; }
-    public int Id { get; set; }
-    public int Level { get; set; }
-    public double Resistance { get; set; }
-    public CharacterPosition Position { get; set; }
-    public bool Dead { get { return this.Hp == 0; } }
+	public double InitHp { get; set; }
+	public double Hp { get; set; }
+	public int Id { get; set; }
+	public int Level { get; set; }
+	public double Resistance { get; set; }
+	public CharacterPosition Position { get; set; }
+	public bool Dead { get { return this.Hp == 0; } }
 
-    public bool IsPlayer { get; set; }
-    public string Username { get; set; }
+	public bool IsPlayer { get; set; }
+	public string Username { get; set; }
 
-    public CharacterClass CharacterClass { get; set; }
+	public CharacterClass CharacterClass { get; set; }
 }
 
 public class CharacterPosition
 {
 
-    public CharacterPosition(float x, float y)
-    {
-        this.x = x;
-        this.y = y;
-    }
+	public CharacterPosition(float x, float y)
+	{
+		this.x = x;
+		this.y = y;
+	}
 
-    public float x { get; set; }
-    public float y { get; set; }
+	public float x { get; set; }
+	public float y { get; set; }
 }
 
 public class Battlefield : Node2D
 {
-    private CharacterNode playerNode;
-    // List of character exclude the current player
-    private List<CharacterNode> characterNodes = new List<CharacterNode>();
+	private CharacterNode playerNode;
+	// List of character exclude the current player
+	private List<CharacterNode> characterNodes = new List<CharacterNode>();
 
-    private CharacterNode genericNodeToClone;
-    private IEnumerable<Character> enemies;
-    private Character player;
+	private CharacterNode genericNodeToClone;
+	private IEnumerable<Character> enemies;
+	private Character player;
 
-    public override void _Ready()
-    {
+	public override void _Ready()
+	{
 
-        this.genericNodeToClone = this.GetNode("NpcNode") as CharacterNode;
-        this.playerNode = this.GetNode("PlayerNode") as CharacterNode;
+		this.genericNodeToClone = this.GetNode("NpcNode") as CharacterNode;
+		this.playerNode = this.GetNode("PlayerNode") as CharacterNode;
 
-        var characters = this.RetrieveConnectedCharacters();
-        this.LoadCharacters(characters);
-        this.AddCharactersSprites();
+		var characters = this.RetrieveConnectedCharacters();
+		this.LoadCharacters(characters);
+		this.AddCharactersSprites();
 
-        WebSocketService webSocketService = WebSocketService.GetInstance();
-        var err = webSocketService.Connect("simple-rpg-kata-ws");
+		WebSocketService webSocketService = WebSocketService.GetInstance();
+		var err = webSocketService.Connect("simple-rpg-kata-ws");
 
-        if (err != Error.Ok)
-        {
-            GD.Print("Unable to connect");
-            this.SetProcess(false);
-        }
-        else
-        {
-            webSocketService.ConnectionClosedEvent += this.ConnectionClosed;
-            webSocketService.ConnectionEnstablishedEvent += this.ConnectionEnstablished;
-            webSocketService.DataReceivedEvent += this.DataChanged;
-        }
+		if (err != Error.Ok)
+		{
+			GD.Print("Unable to connect");
+			this.SetProcess(false);
+		}
+		else
+		{
+			webSocketService.ConnectionClosedEvent += this.ConnectionClosed;
+			webSocketService.ConnectionEnstablishedEvent += this.ConnectionEnstablished;
+			webSocketService.DataReceivedEvent += this.DataChanged;
+		}
 
-    }
+	}
 
-    private void ConnectionEnstablished(object sender, object e)
-    {
-        GD.Print("Connection enstablished");
-    }
+	private void ConnectionEnstablished(object sender, object e)
+	{
+		GD.Print("Connection enstablished");
+	}
 
-    private void ConnectionClosed(object sender, object e)
-    {
-        GD.Print("Connection closed");
-    }
+	private void ConnectionClosed(object sender, object e)
+	{
+		GD.Print("Connection closed");
+	}
 
-    private void DataChanged(object sender, object e)
-    {
-        // For now  this method only receive a list of character position
-        string message = e as string;
-        List<WebSocketParams> webSocketParamsList = JsonConvert.DeserializeObject<List<WebSocketParams>>(message);
+	private void DataChanged(object sender, object e)
+	{
+		// For now  this method only receive a list of character position
+		string message = e as string;
+		List<WebSocketParams> webSocketParamsList = JsonConvert.DeserializeObject<List<WebSocketParams>>(message);
 
-        foreach (var param in webSocketParamsList)
-        {
-            CharacterNode node = this.characterNodes.FirstOrDefault(n => n.Character.Id == param.characterId); // && !n.Character.Dead);
-
-            if (param.isConnected)
-            {
-                if (node != null)
-                    node.UpdateCharacter(param);
-                else
-                    node = this.AddCharacterSprite(new Character() { Id = param.characterId, Hp = param.hp, InitHp = param.initHp, Level = param.level, Position = new CharacterPosition(param.positionX, param.positionY), CharacterClass = (CharacterClass)param.classId });
-
-            }
-            else
-            {
-                this.RemoveChild(node);
-                this.characterNodes.Remove(node);
-            }
-        }
-
-    }
-
-    public override void _Process(float delta)
-    {
-        base._Process(delta);
-        WebSocketService.GetInstance().Poll();
-    }
-
-    private void LoadCharacters(IEnumerable<Character> characters)
-    {
-        this.player = characters.SingleOrDefault(character => character.Username == Session.Username && (int)character.CharacterClass == Session.ClassId);
-        this.enemies = characters.Where(character => character != this.player);
-    }
-
-    private void AddCharactersSprites()
-    {
-        this.playerNode.Initialize(this.player);
-        this.characterNodes.Add(this.playerNode);
-
-        foreach (Character character in this.enemies)
-            this.AddCharacterSprite(character);
-
-    }
-
-    private CharacterNode AddCharacterSprite(Character character)
-    {
-        CharacterNode characterSprite = this.genericNodeToClone.Duplicate() as CharacterNode;
-        this.AddChild(characterSprite);
-
-        characterSprite.Initialize(character);
-
-        characterSprite.Show();
-
-        this.characterNodes.Add(characterSprite);
-
-        return characterSprite;
-    }
-
-    private List<Character> RetrieveConnectedCharacters()
-    {
-        using (HttpClient client = new HttpClient())
-        {
-            HttpResponseMessage response = client.GetAsync("http://simple-rpg-kata.herokuapp.com/api/character/all/connected").Result;
-
-            string json = response.Content.ReadAsStringAsync().Result;
-
-            return JsonConvert.DeserializeObject<List<Character>>(json);
-        }
-    }
-
-    public CharacterNode GetSelectedTarget()
-    {
-        foreach (Node children in this.GetChildren())
-        {
-            if (children is NpcNode)
-            {
-                NpcNode npc = children as NpcNode;
-                if (npc.IsSelected)
-                    return npc;
-            }
-        }
-        return null;
-    }
+		List<CharacterNode> toUpdate = this.characterNodes.Where(x => webSocketParamsList.Select(w => w.characterId).Contains(x.Character.Id)).ToList();
+		List<CharacterNode> toDelete = this.characterNodes.Where(x => !webSocketParamsList.Select(w => w.characterId).Contains(x.Character.Id)).ToList();
+		List<WebSocketParams> toAdd = webSocketParamsList.Where(x => this.characterNodes.Select(w => w.Character.Id).Contains(x.characterId)).ToList();
 
 
-    public void DeselectAllEnemies()
-    {
-        foreach (Node children in this.GetChildren())
-        {
-            if (children is NpcNode)
-            {
-                NpcNode npc = children as NpcNode;
-                npc.Deselect();
-            }
-        }
-    }
+		foreach (CharacterNode node in toUpdate)
+		{
+			WebSocketParams param = webSocketParamsList.SingleOrDefault(x => x.characterId == node.Character.Id);
+			node.UpdateCharacter(param);
+		}
 
-    public void OnNext(Node value)
-    {
-        GD.Print(value);
-        //     throw new NotImplementedException();
-    }
+		foreach (WebSocketParams param in toAdd)
+			this.AddCharacterSprite(new Character() { Id = param.characterId, Hp = param.hp, InitHp = param.initHp, Level = param.level, Position = new CharacterPosition(param.positionX, param.positionY), CharacterClass = (CharacterClass)param.classId });
+		
+		foreach (CharacterNode node in toDelete)
+		{
+			this.RemoveChild(node);
+			this.characterNodes.Remove(node);
+		}
 
-    public void OnError(Exception error)
-    {
-        GD.Print(error);
-        //   throw new NotImplementedException();
-    }
 
-    public void OnCompleted()
-    {
-        GD.Print("on completed");
-        // throw new NotImplementedException();
-    }
+	}
+
+	public override void _Process(float delta)
+	{
+		base._Process(delta);
+		WebSocketService.GetInstance().Poll();
+	}
+
+	private void LoadCharacters(IEnumerable<Character> characters)
+	{
+		this.player = characters.SingleOrDefault(character => character.Username == Session.Username && (int)character.CharacterClass == Session.ClassId);
+		this.enemies = characters.Where(character => character != this.player);
+	}
+
+	private void AddCharactersSprites()
+	{
+		this.playerNode.Initialize(this.player);
+		this.characterNodes.Add(this.playerNode);
+
+		foreach (Character character in this.enemies)
+			this.AddCharacterSprite(character);
+
+	}
+
+	private CharacterNode AddCharacterSprite(Character character)
+	{
+		CharacterNode characterSprite = this.genericNodeToClone.Duplicate() as CharacterNode;
+		this.AddChild(characterSprite);
+
+		characterSprite.Initialize(character);
+
+		characterSprite.Show();
+
+		this.characterNodes.Add(characterSprite);
+
+		return characterSprite;
+	}
+
+	private List<Character> RetrieveConnectedCharacters()
+	{
+		using (HttpClient client = new HttpClient())
+		{
+			HttpResponseMessage response = client.GetAsync("http://simple-rpg-kata.herokuapp.com/api/character/all/connected").Result;
+
+			string json = response.Content.ReadAsStringAsync().Result;
+
+			return JsonConvert.DeserializeObject<List<Character>>(json);
+		}
+	}
+
+	public CharacterNode GetSelectedTarget()
+	{
+		foreach (Node children in this.GetChildren())
+		{
+			if (children is NpcNode)
+			{
+				NpcNode npc = children as NpcNode;
+				if (npc.IsSelected)
+					return npc;
+			}
+		}
+		return null;
+	}
+
+
+	public void DeselectAllEnemies()
+	{
+		foreach (Node children in this.GetChildren())
+		{
+			if (children is NpcNode)
+			{
+				NpcNode npc = children as NpcNode;
+				npc.Deselect();
+			}
+		}
+	}
+
+	public void OnNext(Node value)
+	{
+		GD.Print(value);
+		//     throw new NotImplementedException();
+	}
+
+	public void OnError(Exception error)
+	{
+		GD.Print(error);
+		//   throw new NotImplementedException();
+	}
+
+	public void OnCompleted()
+	{
+		GD.Print("on completed");
+		// throw new NotImplementedException();
+	}
 }
--- client/Scripts/Scene/CharacterNode.cs
+++ client/Scripts/Scene/CharacterNode.cs
@@ -7,92 +7,99 @@ using System.Net.Http;
 
 public abstract class CharacterNode : KinematicBody2D
 {
-    private AnimatedSprite animatedSprite;
+	private AnimatedSprite animatedSprite;
 
-    public Character Character { get; private set; }
-    public Label HpLabel { get; private set; }
-
-
-
-    public override void _Ready()
-    {
-        base._Ready();
-
-        this.HpLabel = this.GetNode("Hp") as Label;
-
-    }
-
-    public void Initialize(Character character)
-    {
-        this.Character = character;
-        this.RenderCharacter();
-        this.CreatePlayerSprite();
-    }
-
-    private void CreatePlayerSprite()
-    {
-        AnimatedSprite animatedSprite = null;
-
-        switch (Character.CharacterClass)
-        {
-            case CharacterClass.Paladin:
-
-                animatedSprite = this.GetNode("/root/Battlefield/WarriorSprite") as AnimatedSprite;
-                break;
-
-            case CharacterClass.Rogue:
-
-                animatedSprite = this.GetNode("/root/Battlefield/RogueSprite") as AnimatedSprite;
-                break;
-
-            case CharacterClass.Wizard:
-
-                animatedSprite = this.GetNode("/root/Battlefield/WizardSprite") as AnimatedSprite;
-                break;
-        }
-
-        //	this.GetParent().RemoveChild(animatedSprite);
-        this.animatedSprite = animatedSprite.Duplicate() as AnimatedSprite;
-        this.animatedSprite.Name = "CharacterSprite";
-        this.animatedSprite.Show();
-        this.AddChild(this.animatedSprite);
-    }
-
-    private void RenderCharacter()
-    {
-        this.Position = new Vector2(Character.Position.x, Character.Position.y);
-        this.RenderLifeStatus();
-    }
-
-    public void RenderLifeStatus()
-    {
-        this.HpLabel.Text = Character.Hp.ToString();
-    }
+	public Character Character { get; private set; }
+	public Label HpLabel { get; private set; }
+	public Label Username { get; private set; }
 
 
 
-    private void Kill()
-    {
-        this.animatedSprite.Play("death");
-        this.SetPhysicsProcess(false);
-        this.Killed();
-    }
+	public override void _Ready()
+	{
+		base._Ready();
+
+		this.HpLabel = this.GetNode("Hp") as Label;
+		this.Username = this.GetNode("Username") as Label;
+
+	}
+
+	public void Initialize(Character character)
+	{
+		this.Character = character;
+		this.RenderCharacter();
+		this.CreatePlayerSprite();
+	}
+
+	private void CreatePlayerSprite()
+	{
+		AnimatedSprite animatedSprite = null;
+
+		switch (Character.CharacterClass)
+		{
+			case CharacterClass.Paladin:
+
+				animatedSprite = this.GetNode("/root/Battlefield/WarriorSprite") as AnimatedSprite;
+				break;
+
+			case CharacterClass.Rogue:
+
+				animatedSprite = this.GetNode("/root/Battlefield/RogueSprite") as AnimatedSprite;
+				break;
+
+			case CharacterClass.Wizard:
+
+				animatedSprite = this.GetNode("/root/Battlefield/WizardSprite") as AnimatedSprite;
+				break;
+		}
+
+		//	this.GetParent().RemoveChild(animatedSprite);
+		this.animatedSprite = animatedSprite.Duplicate() as AnimatedSprite;
+		this.animatedSprite.Name = "CharacterSprite";
+		this.animatedSprite.Show();
+		this.AddChild(this.animatedSprite);
+	}
+
+	private void RenderCharacter()
+	{
+		this.Position = new Vector2(Character.Position.x, Character.Position.y);
+		this.RenderLifeStatus();
+		this.RenderUsername();
+	}
+
+	public void RenderLifeStatus()
+	{
+		this.HpLabel.Text = Character.Hp.ToString();
+	}
+
+	public void RenderUsername()
+	{
+		this.Username.Text = this.Character.Username;
+	}
 
 
-    protected virtual void Killed() { }
+	private void Kill()
+	{
+		this.animatedSprite.Play("death");
+		this.SetPhysicsProcess(false);
+		this.Killed();
+	}
 
-    public virtual void UpdateCharacter(WebSocketParams param)
-    {
-        if (this.Character.Hp > 0 && param.hp == 0)
-            this.Kill();
 
-        this.Character.Position = new CharacterPosition(param.positionX, param.positionY);
-        this.Character.Hp = param.hp;
+	protected virtual void Killed() { }
 
-        this.RenderLifeStatus();
+	public virtual void UpdateCharacter(WebSocketParams param)
+	{
+		if (this.Character.Hp > 0 && param.hp == 0)
+			this.Kill();
 
-        MoveDirection moveDirection = (MoveDirection)param.moveDirection;
-        if (moveDirection == MoveDirection.None)
-            this.Position = new Vector2(param.positionX, param.positionY);
-    }
+		this.Character.Position = new CharacterPosition(param.positionX, param.positionY);
+		this.Character.Hp = param.hp;
+
+		this.RenderLifeStatus();
+
+		MoveDirection moveDirection = (MoveDirection)param.moveDirection;
+		if (moveDirection == MoveDirection.None)
+			this.Position = new Vector2(param.positionX, param.positionY);
+	}
 }
